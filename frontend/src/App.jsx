49c6a57a56f2b8a724f1a293a1e3452903fc3457
Abhishek <programwithabhi@gmail.com>
2025-09-22 19:19:58 +0530

    Set up the basic react app for JobNest

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-red-200 text-center'>JobNest</h1> 
+    </>
+  )
+}
+
+export default App
